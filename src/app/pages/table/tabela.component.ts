a59ab09c36c5cbc9efa26cc00cc3ef7e4625cadd
Adhemar Jr <adhemar.machado@gmail.com>
2024-01-01 08:54:13 -0300

    Remove tabela component and update table component

--- src/app/pages/table/tabela.component.ts
+++ src/app/pages/table/tabela.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  inject,
+} from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-tabela',
+  selector: "app-table",
   standalone: true,
-  imports: [],
-  templateUrl: './tabela.component.html',
-  styleUrl: './tabela.component.scss'
+  imports: [ReactiveFormsModule],
+  templateUrl: "./table.component.html",
+  styleUrl: "./table.component.scss",
 })
 export class TableComponent {
+  public showClass(data: HTMLElement) {
+    const filters = document.querySelectorAll("." + data.className);
+    filters.forEach(function (el, filters) {
+      setTimeout(function () {
+        el.classList.add("animGroup");
+      }, 10 * filters);
+    });
+  }
 
+  public remove(data: HTMLElement) {
+    
+    const filters = document.querySelectorAll(data.className + ", .animGroup");
+
+    filters.forEach(function (el) {
+      el.classList.remove("animGroup");
+    });
+  }
 }
