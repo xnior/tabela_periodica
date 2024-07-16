f3b586965c0980997cdb87b1b0d545462c03c379
Adhemar Jr <adhemar.machado@gmail.com>
2024-07-16 15:56:50 -0300

    Updates

--- chunk-2U456GB3.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-AICBY4R5.js";import"./chunk-KWYVHMPZ.js";import"./chunk-D62SSHD2.js";import"./chunk-LFGDT3NS.js";export{a as TableComponent};
---
+++ chunk-IIUR7ZF3.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-QBI726I7.js";import"./chunk-OFSNCMFX.js";import"./chunk-KM5WFS7S.js";import"./chunk-5HRLELCD.js";export{a as TableComponent};
